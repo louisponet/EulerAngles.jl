262c491fca7bfb9c3fd7b35b3c747d033053f5ac
Guoyuan Liu <liuguoyuan01@outlook.com>
2023-09-15 18:07:20 +0200

    update constructor

--- src/EulerAngles.jl
+++ src/EulerAngles.jl
@@ -2,67 +2,44 @@ module EulerAngles
 using LinearAlgebra
 export Angles
 
-# mutable struct Angles{T, N, VT<:AbstractVector{T}}
-#     θs::VT
-#     r::T
-#     n::NTuple{N, Int}
-# end
-#
-# function Angles(v::AbstractVector)
-#     n = norm(v)
-#     return Angles(angles(v), n, (length(v),))
-# end
-
-struct Angles{T, VVT<:Vector{Vector{T}}}
-    θs::VVT
-    flat::Vector{T}
+mutable struct Angles{T}
+    θs::AbstractVector{T}
+    function Angles(v::AbstractVector{T}) where T
+        n_angles = size(v,1)
+        n_dims = Int((sqrt(8*n_angles+1) - 1) * 0.5)
+        return new{T}(v)
+    end
 end
 
-function toVVA(θs::Vector{Vector{T}}) where T
-    n = length(θs)
+function Angles(n_dim::Int)
+    n_dim < 1 && @error  "dimension of orthogonal matrix should be larger than 1"
+    n_angles::Int = n_dim*(n_dim-1)/2
+    angles = (rand(Float64, n_angles) .- 0.5) * π
+    return Angles(angles)
+end
+
+function layered_to_flat(θl::Vector{Vector{T}}) where T
+    n = length(θl)
     flat = Vector{T}(undef, Int(n*(n+1)//2))
     head = 1
     for (i, len) = zip(1:n, n:-1:1)
-        flat[head:head+len-1] = θs[i]
+        flat[head:head+len-1] = θl[i]
         head += len
     end
-    Angles(θs, flat)
+    return flat
 end
 
-function Angles(flat::Vector{T}) where T
+function flat_to_layered(flat::AbstractVector{T}) where T
     L = size(flat, 1)
     n = Int((sqrt(8*L+1) - 1) * 0.5)
-    θs::Vector{Vector{T}} = []
+    θl::Vector{Vector{T}} = []
     head = 1
     for i =n:-1:1
-        push!(θs, flat[head:head+i-1])
+        push!(θl, flat[head:head+i-1])
         head += i
     end
-    Angles(θs, flat)
+    return θl
 end
-
-# function angles_t(t::Matrix{T}) where T
-#     ν = size(t, 1)
-#     agls = zeros(T, ν)
-#     # last angle is always π/2
-#     agls[ν] = π/2
-#     agls[ν-1] = atan(t[ν-1, ν], t[ν, ν])
-#     if t[ν, ν] / sin(agls[ν-1]) ≈ 1
-#         @info "k = ν-1:"  agls
-#         return agls
-#     end
-#     for k = ν-2:-1:1
-#         if t[k, ν] ≈ 0.
-#             agls[k] = 0 
-#             continue
-#         elseif abs(t[k, ν]) ≈ 1. 
-#             agls[k] = sign(t[k, ν]) * π/2 
-#             return agls
-#         end
-#         agls[k] = atan(t[k, ν] / t[k+1, ν] * sin(agls[k+1]))
-#     end
-#     return agls
-# end
 
 function angles_t(t::Matrix{T}) where T
     ν = size(t, 1)
@@ -126,11 +103,13 @@ function Angles(t::Matrix{T}) where T
         # remove the last angle π/2
         push!(res, agls[1:end-1])
     end
-    return Angles(res)
+    flat = layered_to_flat(res)
+    return Angles(flat)
 end
     
 function Base.Matrix(angles::Angles{T}) where T
-    agls = angles.θs
+    flat = angles.θs
+    agls = flat_to_layered(flat)
     n = size(agls, 1) + 1
     b_mat = diagm(ones(T, n))
     for i = 1:n-1
